5648ec6bb1de9d94a112007f4f7028135614935a
lucasmatheus001 <lucasmatheussm22@gmail.com>
2022-12-02 16:26:59 -0300

    test: add test controller for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,7 @@ RSpec.describe User, type: :model do
       it { is_expected.to validate_uniqueness_of(:email) }
     end
 
+    
+
   end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
 
   describe "GET /index" do
-    it "returns http success" do
-      # get "/users/index"
-      # expect(response).to have_http_status(:success)
+    it "responds successfully" do
+      # get :index
+      # expect(response).to be_success
     end
   end
 

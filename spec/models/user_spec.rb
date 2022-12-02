c8161636bb94bfb5172efb98c2ffd48374f85a88
lucasmatheus001 <lucasmatheussm22@gmail.com>
2022-12-02 15:47:22 -0300

    test: refactoring method add shoulda matcher

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,25 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    it 'should save successfully' do
-      @user = create(:user)
-      expect(@user).to be_valid
-      expect(@user.save).to be_truthy
-    end
-
-    it 'should not save if email nil' do
-      user = build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
-    end
-
-    it 'is invalid without name' do
-      user = build(:user, name: nil)
-      user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
-    end
-
-    it 'is invalid without birthday' do
-      user = build(:user, birthday: nil)
-      user.valid?
-      expect(user.errors[:birthday]).to include("can't be blank")
+    context 'create user' do 
+      it 'should save successfully' do
+        @user = create(:user)
+        expect(@user).to be_valid
+        expect(@user.save).to be_truthy
+      end
     end
     
+    context ' validates' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_presence_of(:cpf) }
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_presence_of(:birthday) }
+    end
+
+    context 'uniqueness' do
+      it { is_expected.to validate_uniqueness_of(:cpf) }
+      it { is_expected.to validate_uniqueness_of(:email) }
+    end
+
   end
 end

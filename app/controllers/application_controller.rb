20ee4275f519c3e17c0f2e2c8aa9b4e1168da9cc
lucasmatheus001 <lucasmatheussm22@gmail.com>
2022-12-03 18:03:52 -0300

    feat: add controller e views home and pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name, :email, :password, :password_confirmation, :cpf, :birthday)
+    end
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_index_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :accounts, dependent: :destroy
+  after_create :create_account
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -10,8 +11,15 @@ class User < ApplicationRecord
   validates :cpf, :name, :email,:birthday, presence: true
   validates :cpf, uniqueness: true
   validates :email, uniqueness: true
-  
 
 
-
+  def create_account
+    loop do
+      account_number = rand(1000000..9999999)
+      unless Account.exists?(number: account_number)
+        Account.create(user_id: self.id, number: account_number, agency: 2448, balance: 0.0)
+        break
+      end
+    end
+  end
 end

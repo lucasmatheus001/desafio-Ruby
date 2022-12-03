c26a56237b371554d4995ebb8f252a5a8edd3f8a
lucasmatheus001 <lucasmatheussm22@gmail.com>
2022-12-02 22:36:55 -0300

    test: add factory to account

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,5 +1,12 @@
+require 'faker'
+require 'user'
 FactoryBot.define do
   factory :account do
-    user { nil }
+    user = create{:user}
+    user_id {user.id}
+    balance {0}
+    number { Faker::Number.number(digits: 10) }
+    agency { Faker::Number.number(digits: 4) }
+    status { 1 }
   end
 end
